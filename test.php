b5e935b23f74aa793d7c443d86de8734474091c0
FarbodFG <mostafaie.fm077@gmail.com>
2024-06-20 17:30:45 +0330

    Initial commit

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'inc/functions.php';
+
+// session_start();
+// $username = $_SESSION['loggedin']; // username
+
+// $getUser = mysqli_query($db, "SELECT * FROM users WHERE username='$username'");
+
+// $userArray = mysqli_fetch_array($getUser);
+
+// echo $userArray['display_name'];
+
+
+echo getUserId();
